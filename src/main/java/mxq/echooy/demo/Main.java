e3d0e9d761caa5441a362ba58693a7bdae3b86f7
echooymxq <echooy.mxq@gmail.com>
2022-10-31 10:52:55 +0800

    commit.

---
+++ src/main/java/mxq/echooy/demo/Main.java
@@ -0,0 +1,17 @@
+package mxq.echooy.demo;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Main {
+
+    private final MessageDigest md = MessageDigest.getInstance("MD5");
+
+    public Main() throws NoSuchAlgorithmException {
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
